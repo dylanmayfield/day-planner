c459690f53106dda3a07be042e445c7b8133d35d
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-09-12 18:12:14 -0500

    updated js and html for timeblock background color to correspond with the actual time

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,14 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+$(function () {
+
 $('.time-block').each(function (index, timeBlock) {
-  const plannedHour = value.getAttribute('data-hour');
-  const actualHour = days().hour();
-  const schedule = {
-    hour: hour,
-    text: text
-  };
+  const plannedHour = timeBlock.getAttribute('data-hour');
+  const actualHour = dayjs().hour();
+  //console.log(actualHour)
+  
 
   if (plannedHour > actualHour) {
     timeBlock.classList.add('future')
@@ -18,47 +19,60 @@ $('.time-block').each(function (index, timeBlock) {
   }
 
   // add event listener
-  timeBlock.addEventListener('click', function (event){
+  timeBlock.addEventListener('click', function (event)  {
     if(event.target.matches('button')) {
       console.log("clicked button");
-      console.log(event.target)
+      console.log(event.target);
+      saveSchedule(event)
     }
   });
+});
+
+// <!-- someElement.id => split('-') => Get [1] -->
+// <!-- Compare that with the hour which is from actualHour-->
+// <!-- If less than, equal, or greater than... that's when you add the right class past, present, or future -->
+// let time = docutment.querySelector(".row time-block past");
+
+
+  // updated schedules
+  for(var i = 0; i<localStorage.length; i++) {
+   
+    const key = localStorage.key(i); // eg. hour-10
+    const localStorageValue = localStorage.getItem(key); // eg. brunch
+  
+    console.log(key)
+    console.log(localStorageValue.trim())
+  
+    document.querySelector("#"+key).querySelector(".description").value = localStorageValue.trim();
+    // console.log(someElement.textContent) // READING from the webpage
+    // someElement.textContent = "Updated text" // WRITING to the webpage
+   } // for
+  
 
   function saveSchedule(event) {
-    const hour = event.target.parentElement.getAttribute('data-hour');
-    const text = event.target.parentElement.queryselector('text-area');
-    console.log('hour', hour)
-    console.log('text', text)
+    const hour = event.target.parentElement.getAttribute("data-hour");
+    const text = event.target.parentElement.querySelector(".description").value;
+ 
+    console.log("hour", hour)
+    console.log("text", text)
+  
 
-    if (localStorage.getItem('schedules')) {
-      const schedules = JSON.parse(localStorage.getItem('schedules'));
-      schedules.push(schedule);
-      localStorage.setItem('schedules', JSON.stringify('schedules'));
-    } else {
-      const schedules = []
-      schedules.push(schedule);
-      localStorage.setItem('schedules',JSON.stringify(schedules));
-    }
-  }
+    localStorage.setItem("hour-"+hour, text)
 
-  $('#currentDay').text(currentDay.format('dddd, MMMM D'))
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  } // saveSchedule
+
+  let dateNow = new Date() // set variable for new date
+  
+  dateNow = dateNow.toString() // converting date into string
+  
+  dateNow = dateNow.split(' ') // converting string to array
+
+  dateNow = dateNow.slice(0,5) // telling what to keep in array
+  
+  dateNow = dateNow.join(' ') // change back to string, added space where items were stringed together
+  // $("#currentDay").text(currentDay.format('dddd, MMMM D'))
+  $("#currentDay").text(dateNow)
+
 });
+ 
+
